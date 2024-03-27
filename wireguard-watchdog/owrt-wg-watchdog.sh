8b3e10e953252e340f2df25e128820368c83bf4d
Erik Conijn <63402314+egc112@users.noreply.github.com>
2024-03-27 11:13:22 +0100

    Update owrt-wg-watchdog.sh

--- wireguard-watchdog/owrt-wg-watchdog.sh
+++ wireguard-watchdog/owrt-wg-watchdog.sh
@@ -2,7 +2,7 @@
 #DEBUG=; set -x # comment/uncomment to disable/enable debug mode
 
 # name: owrt-wg-watchdog.sh
-# version: 0.3, 25-mar-2024, by egc
+# version: 0.31, 26-mar-2024, by egc
 # purpose: WireGuard watchdog with fail-over, by pinging every x seconds through the WireGuard interface, the WireGuard tunnel is monitored,
 #          in case of failure of the WireGuard tunnel the next tunnel is automatically started
 # script type: shell script
@@ -20,14 +20,14 @@
 #    Note the ampersand (&) at the end indicating that the script is executed asynchronously
 # 5. The script can take two parameters, the first the ping time in seconds, default is 30, the second the ip address used for pinging, default 8.8.8.8 
 #    Use a ping time between 10 and 60 seconds, do not set ping time lower than 10 or you run the risk of being banned from the server you are pinging to
-#    As IP address you can use for pinging (default 8.8.8.8) you can also set a host-name which resolves to multiple IP addresses,
+#    Instead of an IP address you use for pinging (default 8.8.8.8) you can also set a host-name which resolves to multiple IP addresses:
 #    under DHCP and DNS > Hostnames (/etc/config/dhcp, config domain) add:
 #    ping-host 8.8.8.8
 #    ping-host 9.9.9.9
-#    Check if the name resolves with nslookup ping-host
-#    Then use ping-host as ping address and all addresses of ping-host will be used in a round robin method, this also adds redundancy if one server is down e.g.:
+#    Check if the name resolves with: nslookup ping-host
+#    Then use ping-host as ping address and all addresses of ping-host will be used in a round robin method, this also adds redundancy if one server is down e.g. start with:
 #    /usr/share/owrt-wg-watchdog.sh 10 ping-host &
-#    This will ping every 10 seconds, after a delay of 120 seconds on startup, to  ping-host (= 8.8.8.8 and 9.9.9.9)
+#    This will ping every 10 seconds (after a delay of 120 seconds on startup) to ping-host (= 8.8.8.8 and 9.9.9.9)
 # 6. reboot
 # 7. View log with: logread -e watchdog, debug by removing the # on the second line of this script, view with: logread | grep debug
 # 8. You can test the script by blocking the endpoint address of a tunnel with:
@@ -39,7 +39,6 @@
 #Add the Wireguard tunnels you want to use for fail over as a continuous range e.g. WG1, WG2 etc., max 9 tunnels
 WG1="name-of-wg1-interface"
 WG2="name-of-wg2-interface"
-#WG3="etc."
 
 #set seconds between log message indicating running watchdog
 alive=3600
@@ -61,6 +60,9 @@ elif ! nslookup "$PINGIP" >/dev/null 2>&1; then
 	echo "WireGuard watchdog ERROR: could not resolve PINGIP $PINGIP"
 	exit 1
 fi
+
+#: ${SLEEP:=30}
+#: ${PINGIP:=8.8.8.8}
 
 activetunnel=1
 
@@ -97,7 +99,7 @@ search_active() {
 		eval "wgi=\$$(echo WG${i})"
 		# check if tunnel exists
 		uci show | grep "interface='$wgi'" 1>/dev/null || echo echo "WireGuard watchdog ERROR: tunnel $wgi does not exist"
-		if ! [[ $(uci -q get network.$wgi.disabled) = "1" ]] >/dev/null 2>&1; then
+		if ! [[ "$(uci -q get network.$wgi.disabled)" = "1" ]] >/dev/null 2>&1; then
 			echo "WireGuard watchdog: tunnel $wgi is enabled"
 			activetunnel=$i
 			wga="$wgi"
