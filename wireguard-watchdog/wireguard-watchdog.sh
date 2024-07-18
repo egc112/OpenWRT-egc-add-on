0ed73b6490f07afee154c9e1613fcfd2bfbe47cd
Erik Conijn <63402314+egc112@users.noreply.github.com>
2024-07-18 13:10:03 +0200

    Update wireguard-watchdog.sh

--- wireguard-watchdog/wireguard-watchdog.sh
+++ wireguard-watchdog/wireguard-watchdog.sh
@@ -2,7 +2,7 @@
 #DEBUG=; set -x # comment/uncomment to disable/enable debug mode
 
 # name: wireguard-watchdog.sh
-# version: 0.93, 14-june-2024, by egc
+# version: 0.95, 14-july-2024, by egc
 # purpose: WireGuard watchdog with fail-over, by pinging every x seconds through the WireGuard interface, the WireGuard tunnel is monitored,
 #          in case of failure of the WireGuard tunnel the next tunnel is automatically started
 #          When the last tunnel has failed, the script will start again with the first tunnel.
@@ -20,16 +20,17 @@
 # 4. To start on startup of the router, add to System > Startup > Local Startup (/etc/rc.local):
 #    /usr/share/wireguard-watchdog.sh &
 #    Note the ampersand (&) at the end indicating that the script is executed asynchronously
-# 5. The script can take two parameters, the first the ping time in seconds, default is 30, the second the ip address used for pinging, default 8.8.8.8 
+# 5  The script can take two parameters, the first the ping time in seconds default is 30, the second the ip address used for pinging,
+#    default is 8.8.8.8
 #    Use a ping time between 10 and 60 seconds, do not set ping time lower than 10 or you run the risk of being banned from the server you are pinging to
 #    Instead of an IP address you use for pinging (default 8.8.8.8) you can also set a host-name which resolves to multiple IP addresses:
 #    under DHCP and DNS > Hostnames (/etc/config/dhcp, config domain) add:
-#    ping-host 8.8.8.8
-#    ping-host 9.9.9.9
-#    Check if the name resolves with: nslookup ping-host
-#    Then use ping-host as ping address and all addresses of ping-host will be used in a round robin method, this also adds redundancy if one server is down e.g. start with:
-#    /usr/share/wireguard-watchdog.sh 10 ping-host &
-#    This will ping every 10 seconds (after a delay of 120 seconds on startup) to ping-host (= 8.8.8.8 and 9.9.9.9)
+#    ping-host.mylan 8.8.8.8
+#    ping-host.mylan 9.9.9.9
+#    Check if the name resolves with: nslookup ping-host.mylan
+#    Then use ping-host.mylan as ping address and all addresses of ping-host.mylan will be used in a round robin method, this also adds redundancy if one server is down e.g. start with:
+#    /usr/share/wireguard-watchdog.sh 10 ping-host.mylan &
+#    This will ping every 10 seconds (after a delay of 120 seconds on startup) to ping-host.mylan (= 8.8.8.8 and 9.9.9.9)
 # 6. reboot
 # 7. View log with: logread -e watchdog, debug by removing the # on the second line of this script, view with: logread | grep debug
 # 8. You can test the script by blocking the endpoint address of a tunnel with:
@@ -63,9 +64,7 @@ elif ! nslookup "$PINGIP" >/dev/null 2>&1; then
 	echo "WireGuard watchdog ERROR: could not resolve PINGIP $PINGIP"
 	exit 1
 fi
-
 activetunnel=1
-
 get_tunnels(){
 	echo -e -n "WireGuard watchdog: Available tunnels: "
 	for i in $(seq 1 9);do 
@@ -75,13 +74,13 @@ get_tunnels(){
 	done
 	echo ""
 }
-
 set_active(){
 	activetunnel=$1
 	[[ $activetunnel -gt $maxtunnels ]] && { activetunnel=1; echo "WireGuard watchdog: all tunnels failed, starting over"; }
 	for i in $(seq 1 "$maxtunnels"); do
 		eval "wgi=\$$(echo WG"${i}")"
 		if [[ $i = "$activetunnel" ]]; then
+			#uci set network."${wgi}".disabled="0"
 			uci -q del network."${wgi}".disabled
 			uci -q del network."${wgi}".auto
 		else
@@ -92,7 +91,6 @@ set_active(){
 	( service network restart >/dev/null 2>&1 ) &
 	sleep 20
 }
-
 search_active() {
 	for i in $(seq "$activetunnel" "$maxtunnels"); do
 		eval "wgi=\$$(echo WG"${i}")"
@@ -106,7 +104,6 @@ search_active() {
 		[[ $i -eq $maxtunnels ]] && set_active 1
 	done
 }
-
 watchdog(){
 	echo "WireGuard watchdog: started, pinging every $SLEEP seconds to $PINGIP on tunnel ${wga} with endpoint $(uci get network.@wireguard_"${wga}"[0].endpoint_host)"
 	while sleep "$SLEEP"; do
@@ -130,6 +127,5 @@ sleep 120	# on startup wait till everything is running
 get_tunnels
 search_active
 watchdog
-
 ) 2>&1 | logger $([ ${DEBUG+x} ] && echo '-p user.debug') \
     -t $(echo $(basename "$0") | grep -Eo '^.{0,23}')[$$] &
